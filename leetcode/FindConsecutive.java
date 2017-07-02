906fda268c669d7ab11d77987688df613915f00b
Shraddha Yeole <shraddhayeole15@gmail.com>
2017-07-01 23:32:12 -0700

    Update FindConsecutive.java

--- leetcode/FindConsecutive.java
+++ leetcode/FindConsecutive.java
@@ -1,38 +1,16 @@
-public class FindConsecutive{
-	
-	public static void main(String args[])
-	{
-		int[] nums={1,1,0,1,1,1,1,1};
-		
-		 int count=1;
-	        int j=0;
-	    	 int[] result=new int[3];
-	        
-	        for(int i=0;i<nums.length;i++)
-	        {
-	            if(nums[i]==1)
-	            {
-	               
-	                result[j]=count++;
-	                
-	            }
-	            else{
-	                
-	                 count=1;
-	                 j++;
-	                continue;
-	               
-	            }
-	        }
-	        
-	        for(int k:result){
-	      	  System.out.println(k);     
-	        }
-	       
-		
-		
-		
-		
-		
-	}
+public class Solution {
+    public int findMaxConsecutiveOnes(int[] nums) {
+        int result = 0;
+        int count = 0;
+        
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == 1) {
+        	count++;
+        	result = Math.max(count, result);
+            }
+            else count = 0;
+        }
+        
+        return result;
+    }
 }
