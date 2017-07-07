2b63d814df88a4cf69ad65620ec05b481e122481
Shraddha Yeole <shraddhayeole15@gmail.com>
2017-07-07 12:09:38 -0700

    Create Reverse String.java

---
+++ leetcode/String/Reverse String.java
@@ -0,0 +1,25 @@
+/*344. Write a function that takes a string as input and returns the string reversed.
+
+Example:
+Given s = "hello", return "olleh".*/
+
+public class Solution {
+    public String reverseString(String s) {
+        
+        char[] result=s.toCharArray();
+        int j=0;
+        while(j<(result.length-1))
+        {
+            j++;
+        }
+        int i=0;
+        while(i<j)
+        {
+            char temp=result[i];
+            result[i++]=result[j];
+            result[j--]=temp;
+        }
+        return String.valueOf(result);
+    }
+    
+}
