4d6cac5ccb799ad38f353826c1b54eef1aad972f
Shraddha Yeole <shraddhayeole15@gmail.com>
2017-07-13 00:05:00 -0700

    Update Single Number.java

--- leetcode/Array/Single Number.java
+++ leetcode/Array/Single Number.java
@@ -4,3 +4,37 @@ Note:
 Your algorithm should have a linear runtime complexity. Could you implement it without using extra memory?*/
 
 
+public class Solution {
+    public int singleNumber(int[] nums) {
+        
+        int result=0;
+        
+        Arrays.sort(nums);
+        if(nums.length==1)
+        {
+            return nums[0];
+        }
+        if(nums.length==0)
+        {
+            return 0;
+        }
+        for(int i=0;i<nums.length-1;i++)
+        {
+            if(nums[i]!=nums[i+1])
+            {
+                result = nums[i];
+            }
+            i++;
+            
+            if(i==(nums.length-1))
+        {
+            return nums[nums.length-1];
+        }
+    }
+        
+        
+        return result;
+    }
+}
+
+/* recommended : bit manipulation */
