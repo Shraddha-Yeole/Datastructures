a43fed2cebe5ca58f7ac2b832e54036772f316c7
Shraddha Yeole <shraddhayeole15@gmail.com>
2017-07-16 23:47:30 -0700

    Update Student Attendance Record I.java

--- leetcode/String/Student Attendance Record I.java
+++ leetcode/String/Student Attendance Record I.java
@@ -8,4 +8,30 @@ A student could be rewarded if his attendance record doesn't contain more than o
 You need to return whether the student could be rewarded according to his attendance record. */
 
 
+public class Solution {
+    public boolean checkRecord(String s) {
+        
+        int countA=0;
+        int continuosL = 0;
+        int charA = 'A';
+        int charL ='L';
+        for(int i=0;i<s.length();i++){
+            if(s.charAt(i) == charA){
+                countA++;
+                continuosL = 0;
+            }
+            else if(s.charAt(i) == charL){
+                continuosL++;
+            }
+            else{
+                continuosL = 0;
+            }
+            if(countA >1 || continuosL > 2 ){
+                return false;
+            }
+        }
+        return true;
+        
+    } 
+}
 
