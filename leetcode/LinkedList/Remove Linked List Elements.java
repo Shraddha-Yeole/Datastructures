16511ca181e81810d89a29bc417cba84ed74f8fe
Shraddha Yeole <shraddhayeole15@gmail.com>
2017-08-05 00:41:10 -0700

    Update Remove Linked List Elements.java

--- leetcode/LinkedList/Remove Linked List Elements.java
+++ leetcode/LinkedList/Remove Linked List Elements.java
@@ -5,4 +5,39 @@ Given: 1 --> 2 --> 6 --> 3 --> 4 --> 5 --> 6, val = 6
 Return: 1 --> 2 --> 3 --> 4 --> 5 */
 
 
-
+public class Solution {
+    public ListNode removeElements(ListNode head, int val) {
+        
+        
+        ListNode fakehead = new ListNode(-1);
+        
+        if(head==null){
+            return head;
+        }
+        
+        else{
+            
+            fakehead.next= head;
+            
+            ListNode temp = head;
+            ListNode prev = fakehead;
+            
+            while(temp!= null ){
+                
+                if(temp.val!=val){
+                    prev = temp;
+                    
+                }
+                else{
+                    prev.next = temp.next;
+                }
+                     
+                temp = temp.next;
+                
+        }
+            }
+        
+        return fakehead.next;
+        
+    }
+}
