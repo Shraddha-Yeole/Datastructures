d332463878a3f3cdb68d312b551d145c4342ab32
Shraddha Yeole <shraddhayeole15@gmail.com>
2017-08-27 22:59:15 -0700

    Update Valid Palindrome.java

--- leetcode/String/Valid Palindrome.java
+++ leetcode/String/Valid Palindrome.java
@@ -9,3 +9,47 @@ Have you consider that the string might be empty? This is a good question to ask
 
 For the purpose of this problem, we define empty string as valid palindrome. */
 
+class Solution {
+    public boolean isPalindrome(String s) {
+        
+        if(s == ""){
+            return true;      
+        }
+        
+        
+        int i=0, j= s.length()-1;
+        
+        while(i<s.length()-1 && j>0)
+        {
+            
+            {
+                if( !Character.isLetterOrDigit(s.charAt(i))){
+                    i++;
+                    
+                }
+                else if( !Character.isLetterOrDigit(s.charAt(j))){
+                    j--;
+                }
+            
+                
+                else{
+                    if(Character.toLowerCase(s.charAt(i)) != Character.toLowerCase(s.charAt(j))){
+                       return false;
+                }
+                    
+                    i++;
+                    j--;
+                    
+                    
+            } 
+                
+            }
+            
+        }
+        
+        
+        
+        return true;
+        
+}
+}
