7bb0c4a2f3a5142377526fb76fa30ba896b6b711
Shraddha Yeole <shraddhayeole15@gmail.com>
2017-08-28 23:49:38 -0700

    Update arrayDs.java

--- Hackerrank-solutions/Array/arrayDs.java
+++ Hackerrank-solutions/Array/arrayDs.java
@@ -22,3 +22,27 @@ Sample Input
 Sample Output
 
 2 3 4 1 */
+
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int arr[] = new int[n];
+        for(int arr_i=0; arr_i < n; arr_i++){
+            arr[arr_i] = in.nextInt();
+        }
+        for(int i=n-1;i>=0;i--){
+            System.out.print(arr[i]+" ");
+        }
+        
+        
+    }
+}
+
