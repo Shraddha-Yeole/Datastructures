3c7b5f465d274f8ea7fdb5c60d99efc142607325
Shraddha Yeole <shraddhayeole15@gmail.com>
2017-09-12 23:38:53 -0700

    Update Add Two Numbers.java

--- leetcode/LinkedList/Add Two Numbers.java
+++ leetcode/LinkedList/Add Two Numbers.java
@@ -9,3 +9,49 @@ Output: 7 -> 0 -> 8
 
 
 
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        
+        
+        ListNode t1 = l1;
+        ListNode t2 = l2;
+        
+        ListNode node = new ListNode(0);
+        
+        ListNode a = node;
+        int sum = 0;
+        
+        
+        while(t1 != null || t2 != null){
+            
+            sum /= 10;
+            
+            if(t1 != null){
+                sum += t1.val;
+                t1 = t1.next;
+            }
+            if(t2 != null){
+                sum +=t2.val;
+                t2 = t2.next;
+                }
+            
+            a.next = new ListNode(sum % 10);
+            a = a.next;
+            
+        }
+        
+        if(sum /10 == 1){
+            a.next = new ListNode(1);
+        }
+        
+        return node.next;
+    }
+}
