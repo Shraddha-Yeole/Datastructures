e874c50719a33584a0bd084053ceea49002cb5af
Shraddha Yeole <shraddhayeole15@gmail.com>
2018-02-12 23:50:39 -0800

    Update climbStairs.java

--- leetcode/Dynamic Programming/climbStairs.java
+++ leetcode/Dynamic Programming/climbStairs.java
@@ -2,5 +2,46 @@
 
 Each time you can either climb 1 or 2 steps. In how many distinct ways can you climb to the top?
 
-Note: Given n will be a positive integer. */
+Note: Given n will be a positive integer. 
+Example 1:
 
+Input: 2
+Output:  2
+Explanation:  There are two ways to climb to the top.
+
+1. 1 step + 1 step
+2. 2 steps
+Example 2:
+
+Input: 3
+Output:  3
+Explanation:  There are three ways to climb to the top.
+
+1. 1 step + 1 step + 1 step
+2. 1 step + 2 steps
+3. 2 steps + 1 step
+
+number of steps required for n steps is addition of n-1 and n-2 steps.
+
+*/
+
+class Solution {
+    public int climbStairs(int n) {
+        
+        int total = 0;
+        int one = 2;
+        int two = 1;
+        
+        if(n<=0) { return 0;}
+        if(n==1) {return 1;}
+        if(n==2) {return 2;}
+        
+        for(int i=2;i<n;i++){
+            total = one+two;
+            two = one;
+            one = total;
+        }
+        return total;
+        
+    }
+}
